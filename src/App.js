b6eb93113ef20fa606e68b73a18a09a3b4d33f97
hyeone999 <caps026@naver.com>
2022-12-26 13:33:56 +0900

    Setting ROW

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import requests from "./API/requests";
 import Banner from "./Components/Banner";
 import Nav from "./Components/Nav";
+import Row from "./Components/Row";
 
 function App() {
   return (
-    <div className="App">
+    <div style={{ background: "#111" }}>
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchURL={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchURL={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchURL={requests.fetchComedyMovies}
+      />
     </div>
   );
 }
---
+++ src/Components/Row.js
@@ -0,0 +1,44 @@
+import axios from "../API/axios";
+import React, { useEffect, useState } from "react";
+import "../CSS/Row.css";
+
+export default function Row({ isLargeRow, title, id, fetchURL }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchURL);
+    console.log("request", request);
+    setMovies(request.data.results);
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              style={{ padding: "25px 0" }}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
+  );
+}

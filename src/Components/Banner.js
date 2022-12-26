7cb2228bcb911da4b1c8037fdc14ea317bda23db
hyeone999 <caps026@naver.com>
2022-12-26 12:36:16 +0900

    modify Banner

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -83,25 +83,10 @@ export default function Banner() {
   }
 }
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  width: 100%
-  height: 100vh
-`;
-
-const HomeContainer = styled.div`
-  width: 100%;
-  height: 100%;
-`;
-
 const Iframe = styled.iframe`
   width: 100%;
   height: 100%;
   z-index: -1;
-  opacity: 0.65;
   border: none;
 
   &::after {
@@ -109,7 +94,21 @@ const Iframe = styled.iframe`
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%
+    width: 100%;
     height: 100%;
   }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100vh;
+`;
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
 `;

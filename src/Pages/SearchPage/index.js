0487ef13a938298e18587c8e3dcdc219a6e465b4
hyeone999 <caps026@naver.com>
2022-12-31 11:05:10 +0900

    Search API

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,5 +1,36 @@
-import React from "react";
+import axios from "../../API/axios";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function SearchPage() {
-  return <div>SearchPage</div>;
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  console.log("useLocation()", useLocation());
+  console.log("searchTerm", searchTerm);
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      console.log(request);
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console("error", error);
+    }
+  };
+
+  return <div></div>;
 }

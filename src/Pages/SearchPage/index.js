ebc3d7f85d131df91b3b508ce9740a6dba892210
hyeone999 <caps026@naver.com>
2022-12-31 11:16:05 +0900

    Devlop Search Function

--- src/Pages/SearchPage/index.js
+++ src/Pages/SearchPage/index.js
@@ -1,6 +1,7 @@
 import axios from "../../API/axios";
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import "./SearchPage.css";
 
 export default function SearchPage() {
   const [searchResults, setSearchResults] = useState([]);
@@ -32,5 +33,35 @@ export default function SearchPage() {
     }
   };
 
-  return <div></div>;
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie" key={movie.id}>
+                <div className="movie__column-poster">
+                  <img
+                    src={movieImageUrl}
+                    alt="movie"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
 }

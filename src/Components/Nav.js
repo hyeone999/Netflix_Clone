85b7af0866221d99c8d208a9858b81a4e2e30f78
hyeone999 <caps026@naver.com>
2023-01-05 09:43:20 +0900

    checking Path

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -27,18 +27,12 @@ export default function Nav() {
     navigate(`/search?q=${e.target.value}`);
   };
 
-  const handleHome = () => {
-    navigate(`main`);
-    window.location.reload();
-  };
-
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
         className="nav__logo"
-        onClick={handleHome}
       />
       <input
         value={searchValue}

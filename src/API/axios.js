604aa6fed393d7fde9b77ef3d62844b9c35495b7
hyeone999 <caps026@naver.com>
2023-01-05 09:30:22 +0900

    security API KEY

--- src/API/axios.js
+++ src/API/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "7c692e47b19ad547d8f0f39365b408f3",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
